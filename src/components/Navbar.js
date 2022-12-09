41bd3900b89a647f575b35eefdcdb219f4a1694e
Eva Olafsdottir <evabergrino@gmail.com>
2022-12-09 00:12:24 -0800

    subscription

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -1,22 +1,27 @@
 
 import React from 'react';
+import navlogo from '../components/navlogo.png';
+
 
 
 const Navbar = () => {
   return (
     <nav className="navbar navbar-expand-lg navbar-dark bg-dark">
     <div className="container-fluid">
-      <a className="navbar-brand" href="#">Musaic</a>
+      <a className="navbar-brand" href="/"><img className='navlogo'src={navlogo} alt='navlogo'/></a>
       <button className="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarNav" aria-controls="navbarNav" aria-expanded="false" aria-label="Toggle navigation">
         <span className="navbar-toggler-icon"></span>
       </button>
       <div className="collapse navbar-collapse" id="navbarNav">
         <ul className="navbar-nav ms-auto">
+        <li className="nav-item">
+          <a className="nav-link text-nowrap" href="/">Pick songs </a>
+          </li>
           <li className="nav-item active">
-            <a className="nav-link" href="#">subscription</a>
+            <a className="nav-link" href="/">plans</a>
           </li>
           <li className="nav-item">
-          <a className="nav-link" href="#">Help</a>
+          <a className="nav-link" href="/">Help</a>
           </li>
         </ul>
       </div>
---
+++ src/components/Subscription.js
@@ -0,0 +1,96 @@
+import React from 'react'
+
+const Subscription = () => {
+  return (
+
+    <div class="container p-5">
+    <div class="row">
+      <div class="col-lg-4 col-md-12 mb-4">
+        <div class="h-100 flip-card">
+          <div class="flip-card-inner">
+            <div class="flip-card-front">
+             
+              <span class="price">$0</span>/month 
+              <h2 class="card-title">Basic</h2>
+              <small></small>
+            </div>
+            <div class="flip-card-back">
+             <ul class="list-group list-group-flush">
+              <li class="list-group-item"><svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor" class="bi bi-check" viewBox="0 0 16 16">
+                <path d="M10.97 4.97a.75.75 0 0 1 1.07 1.05l-3.99 4.99a.75.75 0 0 1-1.08.02L4.324 8.384a.75.75 0 1 1 1.06-1.06l2.094 2.093 3.473-4.425a.267.267 0 0 1 .02-.022z"/>
+              </svg> Cras justo odio</li>
+              <li class="list-group-item"><svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor" class="bi bi-check" viewBox="0 0 16 16">
+                <path d="M10.97 4.97a.75.75 0 0 1 1.07 1.05l-3.99 4.99a.75.75 0 0 1-1.08.02L4.324 8.384a.75.75 0 1 1 1.06-1.06l2.094 2.093 3.473-4.425a.267.267 0 0 1 .02-.022z"/>
+              </svg> Dapibus ac facilisis in</li>
+              <li class="list-group-item"><svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor" class="bi bi-check" viewBox="0 0 16 16">
+                <path d="M10.97 4.97a.75.75 0 0 1 1.07 1.05l-3.99 4.99a.75.75 0 0 1-1.08.02L4.324 8.384a.75.75 0 1 1 1.06-1.06l2.094 2.093 3.473-4.425a.267.267 0 0 1 .02-.022z"/>
+              </svg> Vestibulum at eros</li>
+            </ul>
+            <button class="my-5 btn btn-outline-success btn-lg">Select</button>
+          </div>
+        </div>
+      </div>
+    </div>
+
+    <div class="col-lg-4 col-md-12 mb-5">
+      <div class="h-100 flip-card mb-5">
+        <div class="flip-card-inner">
+          <div class="flip-card-front">
+           
+            <span class="price">$7</span>/month 
+            
+            <h2 class="card-title">Standard</h2>
+            <small></small>
+          </div>
+          <div class="flip-card-back">
+           <ul class="list-group list-group-flush">
+            <li class="list-group-item"><svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor" class="bi bi-check" viewBox="0 0 16 16">
+              <path d="M10.97 4.97a.75.75 0 0 1 1.07 1.05l-3.99 4.99a.75.75 0 0 1-1.08.02L4.324 8.384a.75.75 0 1 1 1.06-1.06l2.094 2.093 3.473-4.425a.267.267 0 0 1 .02-.022z"/>
+            </svg> Cras justo odio</li>
+            <li class="list-group-item"><svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor" class="bi bi-check" viewBox="0 0 16 16">
+              <path d="M10.97 4.97a.75.75 0 0 1 1.07 1.05l-3.99 4.99a.75.75 0 0 1-1.08.02L4.324 8.384a.75.75 0 1 1 1.06-1.06l2.094 2.093 3.473-4.425a.267.267 0 0 1 .02-.022z"/>
+            </svg> Dapibus ac facilisis in</li>
+            <li class="list-group-item"><svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor" class="bi bi-check" viewBox="0 0 16 16">
+              <path d="M10.97 4.97a.75.75 0 0 1 1.07 1.05l-3.99 4.99a.75.75 0 0 1-1.08.02L4.324 8.384a.75.75 0 1 1 1.06-1.06l2.094 2.093 3.473-4.425a.267.267 0 0 1 .02-.022z"/>
+            </svg> Vestibulum at eros</li>
+          </ul>
+          <button class="my-5 btn btn-outline-success btn-lg">Select</button>
+        </div>
+      </div>
+    </div>
+  </div>
+
+
+  <div class="col-lg-4 col-md-12 mb-4">
+    <div class="h-100 flip-card">
+      <div class="flip-card-inner">
+        <div class="flip-card-front">
+         
+          <span class="price">$11</span>/month 
+          
+          <h2 class="card-title">Premium</h2>
+          <small></small>
+        </div>
+        <div class="flip-card-back">
+         <ul class="list-group list-group-flush">
+          <li class="list-group-item"><svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor" class="bi bi-check" viewBox="0 0 16 16">
+            <path d="M10.97 4.97a.75.75 0 0 1 1.07 1.05l-3.99 4.99a.75.75 0 0 1-1.08.02L4.324 8.384a.75.75 0 1 1 1.06-1.06l2.094 2.093 3.473-4.425a.267.267 0 0 1 .02-.022z"/>
+          </svg> Cras justo odio</li>
+          <li class="list-group-item"><svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor" class="bi bi-check" viewBox="0 0 16 16">
+            <path d="M10.97 4.97a.75.75 0 0 1 1.07 1.05l-3.99 4.99a.75.75 0 0 1-1.08.02L4.324 8.384a.75.75 0 1 1 1.06-1.06l2.094 2.093 3.473-4.425a.267.267 0 0 1 .02-.022z"/>
+          </svg> Dapibus ac facilisis in</li>
+          <li class="list-group-item"><svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor" class="bi bi-check" viewBox="0 0 16 16">
+            <path d="M10.97 4.97a.75.75 0 0 1 1.07 1.05l-3.99 4.99a.75.75 0 0 1-1.08.02L4.324 8.384a.75.75 0 1 1 1.06-1.06l2.094 2.093 3.473-4.425a.267.267 0 0 1 .02-.022z"/>
+          </svg> Vestibulum at eros</li>
+        </ul>
+        <button class="my-5 btn btn-outline-success btn-lg">Select</button>
+      </div>
+    </div>
+  </div>
+</div>
+</div>    
+</div>
+  )
+}
+
+export default Subscription
