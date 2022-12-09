d24ce84e1cd615da599d87e08471f397985f7e89
Eva Olafsdottir <evabergrino@gmail.com>
2022-12-08 22:36:18 -0800

    header completed

--- src/App.js
+++ src/App.js
@@ -3,10 +3,14 @@ import './App.css';
 import 'bootstrap/dist/css/bootstrap.css';
 import 'bootstrap/dist/js/bootstrap'
 import Navbar from './components/Navbar';
+import Header from './components/Header';
 
 function App() {
   return (
+    <>
    <Navbar />
+   <Header />
+   </>
   );
 }
 
